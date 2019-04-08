1c154747a5d7c71cf1a9de9042815cf812d94e54
ipsitainfluxiq <ipsita.influxiq@gmail.com>
2019-04-08 18:19:36 +0530

    ips

--- src/app/admindashbord/admindashbord.component.ts
+++ src/app/admindashbord/admindashbord.component.ts
@@ -9,30 +9,29 @@ import { ApiService } from "../api.service";
 })
 export class AdmindashbordComponent implements OnInit {
   datasource:any;
-    pendingmodelapplicationarray1: any=[];
+    pendingapplication_view: any=[];
     brandarray: any=[];
-    // public endpoint = 'datalist';
-    pendingmodelapplicationarray: any=[];
+    model_pending_and_notpending_application_view: any=[];
     notpendingapplication_view: any=[];
-    pendingmodelapplicationarray_skip: any= ['type','password','Contracts_Signed', 'created_at', '_id','username','email','status','date_iso_dateformat','regDate'];
-    pendingmodelapplicationarray_modify_header: any = { 'date': 'Date','Age':'Age','name':'Name','submissionprocess':'Submission Process','contractssigned':'Contracts Signed'};
-    pendingmodelapplicationarray_modify_header1: any = { 'dateformat': 'Date','username':'Username','status':'Status','email':'Email' };
-    pendingmodelapplicationarray_skip1: any= ['_id'];
+    model_pending_and_notpending_application_view_skip: any= ['type','password','Contracts_Signed', 'created_at', '_id','username','email','status','date_iso_dateformat','regDate'];
+    model_pending_and_notpending_application_view_modify_header: any = { 'date': 'Date','Age':'Age','name':'Name','submissionprocess':'Submission Process','contractssigned':'Contracts Signed'};
+    notpendingapplication_view_modify_header1: any = { 'dateformat': 'Date','username':'Username','status':'Status','email':'Email' };
+    pendingapplication_view_skip1: any= ['_id'];
     adminlist:any=[];
+
   constructor(public router: Router,private route: ActivatedRoute, public apiservice: ApiService) {
-    // this.datasource='pendingapplication_view';
-      this.apiservice.getData({'source':'model_pending_and_notpending_application_view'}).subscribe(res=> {
+     /* this.apiservice.getData({'source':'model_pending_and_notpending_application_view'}).subscribe(res=> {
           let result: any;
           result = res;
           console.log('result');
           console.log(result);
           console.log(result.res);
-          this.pendingmodelapplicationarray = result.res;
-          this.pendingmodelapplicationarray[0].Age='';  // extera column add in matHeaderCellDef
-      });
+          this.model_pending_and_notpending_application_view = result.res;
+          this.model_pending_and_notpending_application_view[0].Age='';  // extera column add in matHeaderCellDef
+      });*/
   }
 
-  ngOnInit() {
+ /* ngOnInit() {
 console.log('data in oninit');
       this.route.data.forEach((data) => {
           // PRE LOAD DATA PRIOR
@@ -46,6 +45,21 @@ console.log('data in oninit');
            // this.brandarray1=data['results'].item.notpendingapplication_view;
           this.notpendingapplication_view=data['results'].item.notpendingapplication_view;
       });
-  }
+  }*/
+
+    ngOnInit() {
+        this.route.data.forEach((data) => {
+            // PRE LOAD DATA PRIOR
+            console.log(data);
+            console.log('data from route ... !!!');
+            console.log('json',data['results']);
+            this.pendingapplication_view=data['results'].item.pendingapplication_view;
+            console.log('this.pendingapplication_view');
+            console.log(this.pendingapplication_view);
+            this.brandarray=data['results'].item.brand;
+            this.notpendingapplication_view=data['results'].item.notpendingapplication_view;
+            this.model_pending_and_notpending_application_view=data['results'].item.model_pending_and_notpending_application_view;
+        });
+    }
 
 }
