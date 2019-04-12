95ed9a6a83811b811700ed822dfe0c1219e83a9d
influxiq1 <himadribar.influxiq@gmail.com>
2019-04-12 11:01:55 +0530

    him

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -13,6 +13,5 @@ export const environment = {
     uplodeimg_url: 'https://developmentapi.audiodeadline.com/nodeserver/uploads/modelimages/',
     audio_img_url: 'http://developmentapi.audiodeadline.com:3008/',
     audio_img_folder_url: 'https://developmentapi.audiodeadline.com/nodeserver/uploads/banner/',
-
     environment: 'development'
 };
